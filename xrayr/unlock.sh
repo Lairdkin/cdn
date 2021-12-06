ea5cec934395fefdd69046e1752dd0c8d89c4f52
Lairdkin <84929649+Lairdkin@users.noreply.github.com>
2021-12-06 16:01:08 +0800

    Create unlock.sh

---
+++ xrayr/unlock.sh
@@ -0,0 +1,4 @@
+wget -O /etc/XrayR/custom_outbound.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn@main/xrayr/custom_outbound.json
+wget -O /etc/XrayR/route.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn@main/xrayr/route.json
+sed -i 's/RouteConfigPath:\s#/RouteConfigPath:/' /etc/XrayR/config.yml
+sed -i 's/OutboundConfigPath:\s#/OutboundConfigPath:/' /etc/XrayR/config.yml
