b24729f81ae92cf692fcdb7b00626c36543b8b3f
Lairdkin <84929649+Lairdkin@users.noreply.github.com>
2021-12-06 16:11:58 +0800

    Update unlockhk.sh

--- xrayr/unlockhk.sh
+++ xrayr/unlockhk.sh
@@ -3,4 +3,4 @@ wget -O /etc/XrayR/route.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn@main/xray
 sed -i 's/RouteConfigPath:\s#/RouteConfigPath:/' /etc/XrayR/config.yml
 sed -i 's/OutboundConfigPath:\s#/OutboundConfigPath:/' /etc/XrayR/config.yml
 /usr/bin/XrayR restart
-echo -e "\033[32m 区域解锁完成 \033[0m" 
+echo -e "\033[32mHK区域解锁完成 \033[0m" 
