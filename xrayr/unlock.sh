739b569e9edbeb8b28e68eaacbd2b3405aa41bdb
Lairdkin <84929649+Lairdkin@users.noreply.github.com>
2021-12-06 16:05:47 +0800

    Update unlock.sh

--- xrayr/unlock.sh
+++ xrayr/unlock.sh
@@ -2,3 +2,4 @@ wget -O /etc/XrayR/custom_outbound.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn
 wget -O /etc/XrayR/route.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn@main/xrayr/route.json
 sed -i 's/RouteConfigPath:\s#/RouteConfigPath:/' /etc/XrayR/config.yml
 sed -i 's/OutboundConfigPath:\s#/OutboundConfigPath:/' /etc/XrayR/config.yml
+echo -e "\033[32m 解锁完成 \033[0m" 
