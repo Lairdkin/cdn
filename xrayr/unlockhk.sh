00b0d222315c8a9d6f457a2f0b8b44e462e1304d
Lairdkin <84929649+Lairdkin@users.noreply.github.com>
2022-01-06 21:47:03 +0800

    Update unlockhk.sh

--- xrayr/unlockhk.sh
+++ xrayr/unlockhk.sh
@@ -1,5 +1,5 @@
-wget -O /etc/XrayR/custom_outbound.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn@main/xrayr/custom_outbound.json
-wget -O /etc/XrayR/route.json https://cdn.jsdelivr.net/gh/Lairdkin/cdn@main/xrayr/route.json
+wget -O /etc/XrayR/custom_outbound.json https://raw.githubusercontent.com/Lairdkin/cdn/main/xrayr/custom_outbound.json
+wget -O /etc/XrayR/route.json https://raw.githubusercontent.com/Lairdkin/cdn/main/xrayr/route.json
 sed -i 's/RouteConfigPath:\s#/RouteConfigPath:/' /etc/XrayR/config.yml
 sed -i 's/OutboundConfigPath:\s#/OutboundConfigPath:/' /etc/XrayR/config.yml
 /usr/bin/XrayR restart
