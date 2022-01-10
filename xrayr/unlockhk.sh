28510a048adcbdcc59d328d4b08017f252fd4e0a
Lairdkin <84929649+Lairdkin@users.noreply.github.com>
2022-01-10 17:19:04 +0800

    Update unlockhk.sh

--- xrayr/unlockhk.sh
+++ xrayr/unlockhk.sh
@@ -1,6 +1,106 @@
-wget -O /etc/XrayR/custom_outbound.json https://raw.githubusercontent.com/Lairdkin/cdn/main/xrayr/custom_outbound.json
-wget -O /etc/XrayR/route.json https://raw.githubusercontent.com/Lairdkin/cdn/main/xrayr/route.json
-sed -i 's/RouteConfigPath:\s#/RouteConfigPath:/' /etc/XrayR/config.yml
-sed -i 's/OutboundConfigPath:\s#/OutboundConfigPath:/' /etc/XrayR/config.yml
-/usr/bin/XrayR restart
-echo -e "\033[32mHK区域解锁完成 \033[0m" 
+FontColor_Red="\033[31m"
+FontColor_Red_Bold="\033[1;31m"
+FontColor_Green="\033[32m"
+FontColor_Green_Bold="\033[1;32m"
+FontColor_Yellow="\033[33m"
+FontColor_Yellow_Bold="\033[1;33m"
+FontColor_Purple="\033[35m"
+FontColor_Purple_Bold="\033[1;35m"
+FontColor_Suffix="\033[0m"
+
+
+log() {
+    local LEVEL="$1"
+    local MSG="$2"
+    case "${LEVEL}" in
+    INFO)
+        local LEVEL="[${FontColor_Green}${LEVEL}${FontColor_Suffix}]"
+        local MSG="${LEVEL} ${MSG}"
+        ;;
+    WARN)
+        local LEVEL="[${FontColor_Yellow}${LEVEL}${FontColor_Suffix}]"
+        local MSG="${LEVEL} ${MSG}"
+        ;;
+    ERROR)
+        local LEVEL="[${FontColor_Red}${LEVEL}${FontColor_Suffix}]"
+        local MSG="${LEVEL} ${MSG}"
+        ;;
+    *) ;;
+    esac
+    echo -e "${MSG}"
+}
+
+download_config() {
+      local regin="$1"
+      wget -O /etc/XrayR/custom_outbound.json https://raw.githubusercontent.com/Lairdkin/cdn/main/xrayr/custom_outbound_${regin}.json
+      wget -O /etc/XrayR/route.json https://raw.githubusercontent.com/Lairdkin/cdn/main/xrayr/route_${regin}.json
+      sed -i 's/RouteConfigPath:\s#/RouteConfigPath:/' /etc/XrayR/config.yml
+      sed -i 's/OutboundConfigPath:\s#/OutboundConfigPath:/' /etc/XrayR/config.yml
+      log INFO "配置文件下载完毕"
+}
+
+
+unlockhk() {
+      donload_config hk
+      /usr/bin/XrayR restart
+      log INFO "香港流媒体解锁配置完成"
+}
+
+
+unlockjp() {
+      bash <(curl -fsSL git.io/warp.sh) proxy
+      donload_config jp
+      /usr/bin/XrayR restart
+      log INFO "日本流媒体解锁配置完成"
+}
+
+
+unlockus() {
+      bash <(curl -fsSL git.io/warp.sh) proxy
+      donload_config us
+      /usr/bin/XrayR restart
+      log INFO "美国流媒体解锁配置完成"
+}
+
+
+unlocksg() {
+      bash <(curl -fsSL git.io/warp.sh) proxy
+      donload_config jp
+      /usr/bin/XrayR restart
+      log INFO "新加坡流媒体解锁配置完成"
+}
+
+unlockwarp() {
+      bash <(curl -fsSL git.io/warp.sh) proxy
+      donload_config warp
+      /usr/bin/XrayR restart
+      log INFO "warp解锁配置完成"
+}
+
+
+if [ $# -ge 1 ]; then
+    case ${1} in
+    hk)
+        unlockhk
+        ;;
+    jp)
+        unlockjp
+        ;;
+    sg)
+        unlocksg
+        ;;
+    us)
+        unlockus
+        ;;
+    warp)
+        unlockwarp
+        ;;
+    *)
+        log ERROR "Invalid Parameters: $*"
+        Print_Usage
+        exit 1
+        ;;
+    esac
+else
+    Print_Usage
+fi
